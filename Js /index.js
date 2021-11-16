6fadde74045f3a257c30dcaeefd5882278b9d406
ArfI Idris <idrisarfi@gmail.com>
2021-11-16 11:56:31 +0300

    add changes index.js

--- Js /index.js
+++ Js /index.js
@@ -9,28 +9,28 @@ document.getElementById("btn").addEventListener("click",function(){
   var week=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
   var day=week[new Date(year.value + "/" + month.value + "/" + dates.value).getDay()]
  if(gender.value === "male"){
-   if(day="Sunday"){
+   if(day==="Sunday"){
      name="Kwasi"
    }
-   else if(day="Monday"){
+   else if(day==="Monday"){
      name="Kwadwo"
    }
-   else if(day="Tuesday"){
+   else if(day==="Tuesday"){
     name="Kwabena"
   }
-  else if(day="Wednesday"){
+  else if(day==="Wednesday"){
     name="Kwaku"
   }
-  else if(day="Thursday"){
+  else if(day==="Thursday"){
     name="Yaw"
   }
-  else if(day="Friday"){
+  else if(day==="Friday"){
     name="Kofi"
   }
-  else if(day="Saturday"){
+  else if(day==="Saturday"){
     name="Kwame"
   }
-   
- }
   
+ }
+ console.log(name)
 })
