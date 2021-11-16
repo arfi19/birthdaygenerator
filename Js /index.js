f0af1d1981861dcf0026914ef122ff66c13fa9d6
ArfI Idris <idrisarfi@gmail.com>
2021-11-16 11:40:57 +0300

    add option

--- Js /index.js
+++ Js /index.js
@@ -1,3 +1,11 @@
+
+var year=document.getElementById("year")
+var month=document.getElementById("month")
+var dates=document.getElementById("dates")
+
 document.getElementById("btn").addEventListener("click",function(){
-  console.log("I was clicked")
+  var week=['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
+  var day=week[new Date(year.value + "/" + month.value + "/" + dates.value).getDay()]
+  console.log(day)
+  
 })
